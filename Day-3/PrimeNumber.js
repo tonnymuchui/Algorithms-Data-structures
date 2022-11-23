e39f7d6bf1b0cef6d22a650a119212eaa3752595
tonnymuchui <tonykanyingah@gmail.com>
2022-11-23 09:37:21 +0300

    Solve Prime number algorithm

---
+++ Day-3/PrimeNumber.js
@@ -0,0 +1,11 @@
+function prime(n) {
+    if (n < 2) {
+      return false;
+    }
+    for(let i = 2; i < n; i++) {
+      if(n%i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
